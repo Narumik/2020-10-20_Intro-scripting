57dd500c2fe6145cd250e187cd4fb4cc85b2bcd6
Narumik <contact.narumik@gmail.com>
2020-10-21 11:03:37 +0200

    script simpleMove

---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    public Transform target;
+    public float vitesse = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 deplacement = target.position - transform.position;
+        deplacement = deplacement.normalized * vitesse * Time.deltaTime;
+        transform.position += deplacement;
+    }
+}
